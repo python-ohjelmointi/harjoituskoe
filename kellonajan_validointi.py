c41185fd1d489f5aada5440ade68a43a595ed4a0
valeriagalushkina <165795654+valeriagalushkina@users.noreply.github.com>
2024-12-05 14:58:06 +0200

    Update kellonajan_validointi.py

--- kellonajan_validointi.py
+++ kellonajan_validointi.py
@@ -1,67 +1,36 @@
-"""
-Kellonajan validointi
+def validoi_aika(aika: str) -> bool:
+    """
+    Tarkistaa, onko annettu aika kelvollinen 24 tunnin kellonajan muodossa.
+    
+    Palauttaa True, jos aika on kelvollinen, muuten False.
+    """
+    if len(aika) < 4 or len(aika) > 5:
+        return False
+    if ':' not in aika:
+        return False
+    osat = aika.split(':')
+    if len(osat) != 2:
+        return False
+    
+    tunti, minuutti = osat
+    
+    if not tunti.isdigit() or not minuutti.isdigit():
+        return False
 
-Tehtäväsi on toteuttaa uusi funktio `validoi_aika`, joka tarkistaa, onko sille
-annettu aika kelvollinen.
-
-Ajat annetaan merkkijonoina ja niiden tulee olla muodossa "h:mm", missä h on
-tunnit ja mm on minuutit. Ajan tulee olla kelvollinen 24 tunnin muodossa.
-
-Mikäli annettu aika on kelvollinen, funktion tulee palauttaa True. Muussa
-tapauksessa sen tulee palauttaa False. Funktio ei saa aiheuttaa poikkeusta,
-vaikka annettu merkkijono olisi epäkelpo.
-
-Esimerkit:
-
-Seuraavat ajankohdat ovat kelvollisia, joten funktion tulee palauttaa True:
-
-    >>> validoi_aika("0:10")
-    True
-
-    >>> validoi_aika("23:59")
-    True
-
-
-Seuraaville kellonajoille palautetaan False, koska ne ovat epäkelpoja:
-
-    >>> validoi_aika("30:30")
-    False
-
-    >>> validoi_aika("12:61")
-    False
-
-
-Funktio ei saa aiheuttaa virhettä, vaikka annettu muoto olisi virheellinen
-tai puutteellinen:
-
->>> validoi_aika("0:1")
-False
-
->>> validoi_aika("10")
-False
-
->>> validoi_aika(":")
-False
-
->>> validoi_aika("10:01:")
-False
-
-
-Jos kirjoitat omia testejä tai kokeiluja, toteuta ne if __name__
--lohkon sisään. Voit halutessasi suorittaa yllä esitetyt doctest-
-testit komennolla:
-
-python3 -m doctest --verbose kellonajan_validointi.py
-"""
-
-
-
-# Toteuta tänne uusi funktio, jonka nimi, parametrit ja paluuarvot
-# noudattavat tehtävänantoa.
-
+    tunti = int(tunti)
+    minuutti = int(minuutti)
+    if 0 <= tunti <= 23 and 0 <= minuutti <= 59:
+        return True
+    return False
 
 if __name__ == "__main__":
-    """
-    Kirjoita mahdolliset omat testit ja kokeilut tähän lohkoon.
-    Voit myös halutessasi poistaa tämän if-lohkon.
-    """
+
+    print(validoi_aika("0:10"))   # True
+    print(validoi_aika("23:59"))  # True
+    print(validoi_aika("30:30"))  # False
+    print(validoi_aika("12:61"))  # False
+    print(validoi_aika("0:1"))    # False
+    print(validoi_aika("10"))     # False
+    print(validoi_aika(":"))      # False
+    print(validoi_aika("10:01:")) # False
+
