9b1db9edda6ce66cf11abd37192bb7fe35e48ca1
valeriagalushkina <165795654+valeriagalushkina@users.noreply.github.com>
2024-12-05 14:59:47 +0200

    Update perakkaiset.py

--- perakkaiset.py
+++ perakkaiset.py
@@ -1,51 +1,27 @@
-"""
-Peräkkäisten numeroiden tarkistus
-
-Tehtäväsi on toteuttaa tarkista_perakkaiset-funktio, joka tarkistaa,
-sisältääkö sille annettu järjestämätön kokonaislukujen lista peräkkäisiä
-numeroita. Numerot saavat olla missä tahansa järjestyksessä, kunhan ne
-muodostavat yhdessä peräkkäisten lukujen sarjan. Yksikään luku ei saa
-esiintyä enempää kuin kerran, ja voit olettaa, että annettu lista ei ole
-koskaan tyhjä.
-
-Esimerkki:
-
-    >>> tarkista_perakkaiset([1, 2, 3, 4, 5])
-    True
-
-    >>> tarkista_perakkaiset([5, 4, 3, 2, 1])
-    True
-
-Luvut saavat olla missä vain järjestyksessä, eli myös tämä tapaus sallitaan:
-
-    >>> tarkista_perakkaiset([1, 3, 2, 5, 4])
-    True
-
-Yksikään luku ei kuitenkaan saa puuttua, eikä esiintyä useammin kuin kerran:
-
-    >>> tarkista_perakkaiset([5, 3, 1])
-    False
-
-    >>> tarkista_perakkaiset([5, 3, 2, 2, 1])
-    False
-
-Jos kirjoitat omia testejä tai kokeiluja, toteuta ne if __name__
--lohkon sisään. Voit halutessasi suorittaa yllä esitetyt doctest-
-testit komennolla:
-
-python3 -m doctest --verbose perakkaiset.py
-"""
-
-
-
 def tarkista_perakkaiset(numerot: list) -> bool:
     """
-    Toteuta logiikka, joka tarkastaa annetun `numerot`-listan ja
-    palauttaa tehtävänannon mukaisesti totuusarvon True tai False.
+    Tarkistaa, muodostavatko annetut numerot peräkkäisten lukujen sarjan.
+    
+    Palauttaa True, jos numerot ovat peräkkäisiä ilman puuttuvia tai toistuvia lukuja,
+    muuten False.
     """
+    numerot_set = set(numerot)
+    
+    if len(numerot_set) != len(numerot):
+        return False
+    
+    min_luku = min(numerot_set)
+    max_luku = max(numerot_set)
+    
+    if max_luku - min_luku == len(numerot_set) - 1:
+        return True
+    
+    return False
 
 if __name__ == "__main__":
-    """
-    Kirjoita mahdolliset omat testit ja kokeilut tähän lohkoon.
-    Voit myös halutessasi poistaa tämän if-lohkon.
-    """
+    
+    print(tarkista_perakkaiset([1, 2, 3, 4, 5]))  # True
+    print(tarkista_perakkaiset([5, 4, 3, 2, 1]))  # True
+    print(tarkista_perakkaiset([1, 3, 2, 5, 4]))  # True
+    print(tarkista_perakkaiset([5, 3, 1]))        # False
+    print(tarkista_perakkaiset([5, 3, 2, 2, 1]))  # False
